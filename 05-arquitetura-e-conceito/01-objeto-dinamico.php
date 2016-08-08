bfe7ee344aa30bcbe9eca1b54c6044dc3aa08129
thierry rene matos <td_matos@terra.com.br>
2016-08-08 03:32:22 +0000

    update

---
+++ 05-arquitetura-e-conceito/01-objeto-dinamico.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>treinando php</title>
+    </head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cyborg/bootstrap.min.css" type="text/css" />
+
+<body class="container" style="margin-top:100px;">
+    <?php
+        require('./inc/Config.inc.php');
+        
+        
+    ?>
+</body>
+
+</html>
