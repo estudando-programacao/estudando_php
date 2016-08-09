9e0e8b770d8388c339a5ade668052d23576833fc
thierry rene matos <td_matos@terra.com.br>
2016-08-09 18:20:59 +0000

    update

--- 05-tratamento-de-erros/01-tipos-de-validacao.php
+++ 05-tratamento-de-erros/01-tipos-de-validacao.php
@@ -13,7 +13,7 @@
         // teste de validação por existência, verifica se item existe e se valor corresponde ao tipo validado
         $string = "string";
         // valor inválido apenas para validar teste
-        $string = 10;
+        // $string = 10;
         
         // exemplo de validação de string com if else
         if(is_string($string)):
@@ -22,13 +22,33 @@
             echo "{$string} não é uma string.";
         endif;
         
+        echo "<br>";
+        
         // validação para verificar se a string está em branco
         if(!empty($string)):
             echo "A string existe e tem valor";
-        else (isset($string)):
+        elseif (isset($string)):
             echo "A string existe mas não tem valor definido";
         endif;
+        ?>
         
+        <hr>
+        
+        <h3>tomada de decisão</h3>
+        
+        <?php
+        // este tipo de decisão pode ser muito útil quando você sabe os erros que o sistema deve aprender com base em ações do usuário.
+        $decisao = "test@teste.com";
+        
+        // condição para verificar se valor é de fato um email.
+        if(!filter_var($decisao, FILTER_VALIDATE_EMAIL)):
+            echo "Email informado é inválido";
+        // note que aqui a aplicação toma um die se o email for igual ao informado na condição
+        elseif($decisao == "teste@teste.com"):
+            die("Email inválido.<br>A aplicação parou.");
+        else:
+            echo "Email válido";
+        endif;
         ?>
         
 </body>  
