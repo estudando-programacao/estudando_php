a111a0989da133affdf7cedf634d3fa9055f00e8
thierry rene matos <td_matos@terra.com.br>
2016-08-09 17:28:31 +0000

    update - finalizado modelo de composicao

---
+++ 04-arquitetura-e-conceito/class/ComposicaoUsuario.class.php
@@ -0,0 +1,30 @@
+<?php
+
+class ComposicaoUsuario {
+    public $Nome;
+    public $Email;
+    private $Endereco;
+    
+    function __construct($Nome, $Email) {
+        $this->Nome = $Nome;
+        $this->Email = $Email;
+    }
+    
+    // note o conceito de compisção sendo aplicado, ao declararmos um objeto dentro de uma classe e em seguida utilizando seus métodos
+    public function CadastrarEndereco($Cidade, $Estado) {
+        $this->Endereco = new ComposicaoEndereco($Cidade, $Estado);
+    }
+    
+    public function getEndereco() {
+        return $this->Endereco;
+    }
+    
+    public function getNome() {
+        return $this->Nome;
+    }
+    
+    public function getEmail() {
+        return $this->Email;
+    }
+
+}
