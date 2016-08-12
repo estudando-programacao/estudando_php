87219c5948a99d9e10fe7d47e09a66f8afde867c
thierry rene matos <td_matos@terra.com.br>
2016-08-12 12:59:29 +0000

    update - iniciado modulo PHP DATA OBJECT

---
+++ 06-php-data-object/01-conectando-ao-banco.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>treinando php</title>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cyborg/bootstrap.min.css"/>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script type="text/javascript">
+        $('document').ready() {
+            $('body').css("margin-top", "50px");   
+        }
+    </script>
+</head>
+<body class="container">
+    
+    <p class="text-center">PDO - PHP Data Object</p>        
+       
+    <?php
+    require ('./_app/Config.inc.php');
+    
+    
+        
+    ?>
+    
+</body>  
+</html>
