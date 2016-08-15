1f464460b0f45a5d97fd742c4347243c9d2665ff
Thierry Rene <td_matos@msn.com>
2016-08-15 16:08:31 -0300

    update

--- 06-php-data-object/03-stored-procedures.php
+++ 06-php-data-object/03-stored-procedures.php
@@ -20,23 +20,35 @@
       $Query = "SELECT * FROM  ws_siteviews_agent WHERE agent_name = :name";
       $Exe = $Conn->getConn()->prepare($Query);
 
+      // STORED PROCEDURE para trazer resultados com agent_name igual a Chrome
       $Exe->bindValue(":name", "Chrome");
       $Exe->execute();
       // fetchAll retorna todos os valores resultantes da query, de acordo com o indice
       $Chrome = $Exe->fetchAll(PDO::FETCH_ASSOC);
 
+      // debug se $Chrome conter algo
+      if($Chrome):
+        var_dump($Chrome);
+      endif;
+
+      echo "<hr>";
+
+      // STORED PROCEDURE para retornar valores com agent_name igual a Safari
       $Exe->bindValue(":name", "Safari");
       $Exe->execute();
       // fetch retorna apenas um valor da query, útil quando a intenção é apenas retornar um valor (avisos, por exemplo).
+      // note que aqui declaramos apenas FETCH, o que indica o retorno de apenas um valor / na tabela do BD constam dois resultados com name_agent igual a Safari
       $Safari = $Exe->fetch(PDO::FETCH_ASSOC);
 
+      // debug se $Safari conter algo
+      if($Safari):
+        var_dump($Safari);
+      endif;
+
     } catch (PDOException $e) {
+      // apresentamos falha caso seja ocorra algum erro no PDO
       PHPErro($e->getCode(), $e->getMessage(), $e->getFile(), $e->getLine());
     }
-
-    if($Chrome):
-      var_dump($Chrome);
-    endif;
 
     ?>
 </body>
