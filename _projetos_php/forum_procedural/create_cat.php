1ab514de72ee2dda25434d7fb9b21c2c93c3f279
Thierry Rene <td_matos@msn.com>
2016-08-24 11:09:59 -0300

    update

--- _projetos_php/forum_procedural/create_cat.php
+++ _projetos_php/forum_procedural/create_cat.php
@@ -1,5 +1,4 @@
 <?php
-
 include 'connect.php';
 include 'header.php';
 
@@ -17,35 +16,34 @@ include 'header.php';
 //
 
 echo '<div class="container">';
+?>
 
-if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-    //the form hasn't been posted yet, display it
-    echo '<form method="post" action="" class="form-group">
-            <div class="form-group">
-                <label for="cat_name">Nome da categoria</label>
-                <input type="text" name="cat_name" class="form-control" id="cat_name" placeholder="entre com o nome de usuário">
-            </div>
-            <div class="form-group">
-                <label for="cat_description">Descrição da categoria</label>
-                <textarea  name="cat_description" class="form-control" id="cat_description"></textarea>
-            </div>
-            <div class="form-group">
-                <input type="submit" value="Adicionar categoria" class="btn btn-default">
-            </div>
-        </form>';
+<form method="post" action="" class="form-group">
+    <div class="form-group">
+        <label for="cat_name">Nome da categoria</label>
+        <input type="text" name="cat_name" class="form-control" id="cat_name">
+    </div>
+    <div class="form-group">
+        <label for="cat_description">Descrição da categoria</label>
+        <textarea  name="cat_description" class="form-control" id="cat_description"></textarea>
+    </div>
+    <div class="form-group">
+        <input type="submit" value="Adicionar categoria" class="btn btn-default">
+    </div>
+</form>
+
+<?php
+$sql = "INSERT INTO categories(cat_name, cat_description) VALUES('" . mysql_real_escape_string($_POST['cat_name']) . "','" . mysql_real_escape_string($_POST['cat_description']) . "')";
+$result = mysql_query($sql);
+if (!$result) {
+    echo '<div class="alert alert-danger">Ocorreu um erro, tente novamente.</div>';
+    echo mysql_error();
 } else {
-    $sql = "INSERT INTO categories(cat_name, cat_description) VALUES('" . mysql_real_escape_string($_POST['cat_name']) . "','" . mysql_real_escape_string($_POST['cat_description']) . "')";
-
-    $result = mysql_query($sql);
-    if (!$result) {
-        echo '<div class="alert alert-danger">Ocorreu um erro, tente novamente.</div>';
-        echo mysql_error();
-    } else {
-        echo '<div class="container alert alert-success">Nova Categoria adicionada com sucesso.</div>';
-    }
+    echo '<div class="container alert alert-success">Nova Categoria adicionada com sucesso.</div>';
 }
 
-echo '</div>';
-include 'footer.php';
 
+echo '</div>';
+
+include 'footer.php';
 ?>
